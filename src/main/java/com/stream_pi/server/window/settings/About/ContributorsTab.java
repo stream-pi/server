6c0401199950d19836cec50b2857db6234b3ec1c
Debayan Sutradhar <debayansutradhar3@gmail.com>
2021-05-30 22:28:46 +0530

    Updated license

--- src/main/java/com/stream_pi/server/window/settings/About/ContributorsTab.java
+++ src/main/java/com/stream_pi/server/window/settings/About/ContributorsTab.java
@@ -67,7 +67,7 @@ public class ContributorsTab extends VBox
 
         Label disclaimerLabel = new Label("This contributor list shows only those who have contributed " +
                 "to the Server Source code.\nTo know about the contributors of Action API, Theme API, Util, " +
-                "visit the respective repositories.");
+                "visit the respective repositories. If you want to know about the Core Team instead, please visit the website.");
 
         disclaimerLabel.getStyleClass().add("about_license_contributors_disclaimer_label");
 
