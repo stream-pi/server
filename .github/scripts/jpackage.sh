7b1b804d7ecc13351f79d5530ee7297642a0c632
Debayan Sutradhar <debayansutradhar3@gmail.com>
2021-07-06 16:42:31 +0530

    removed append lim flag for macos for start at boot

--- .github/scripts/jpackage.sh
+++ .github/scripts/jpackage.sh
@@ -1,5 +1,4 @@
 $JPACKAGE_HOME/bin/jpackage \
---add-launcher run_min=assets/run_min_linux_and_macos.properties \
 --module-path $JAVAFX_JMODS/:target/lib/ \
 --add-modules $REQ_MODULES \
 --input target/lib \
@@ -12,5 +11,4 @@ $JPACKAGE_HOME/bin/jpackage \
 --dest $INSTALL_DIR \
 --name 'Stream-Pi Server' \
 --java-options '-Dprism.verbose=true -Djavafx.verbose=true -Dprism.dirtyopts=false' \
---arguments 'Stream-Pi.startupRunnerFileName=run_min Stream-Pi.appendPathBeforeRunnerFileToOvercomeJPackageLimitation=true' \
 "$@"
