759d8c186efde8948232db987b8c49bd47d5a034
Debayan Sutradhar <debayansutradhar3@gmail.com>
2021-10-23 15:09:45 +0530

    update license

--- src/main/java/com/stream_pi/server/info/License.java
+++ src/main/java/com/stream_pi/server/info/License.java
@@ -40,6 +40,7 @@ public class License {
             "2. JSON - The JSON License\nhttps://www.json.org/license.html\n\n"+
             "3. Ikonli - Apache License\nhttps://github.com/kordamp/ikonli/blob/master/LICENSE\n\n"+
             "4. Roboto Font - Apache License 2.0\nhttp://www.apache.org/licenses/LICENSE-2.0\n\n" +
-            "5. ControlsFX - BSD 3-Clause License\nhttps://github.com/controlsfx/controlsfx/blob/jfx-13/license.txt\n\n";
+            "5. ControlsFX - BSD 3-Clause License\nhttps://github.com/controlsfx/controlsfx/blob/jfx-13/license.txt\n\n" +
+            "6. Medusa - Apache License 2.0 https://github.com/HanSolo/medusa/blob/master/LICENSE";
     }
 }
