52b926a79c0b94d8d81e767fe0f12550c2dc9923
Debayan Sutradhar <debayansutradhar3@gmail.com>
2021-10-30 23:20:12 +0530

    Add Copyright header

--- src/main/java/com/stream_pi/server/window/dashboard/actiondetailspane/ActionDetailsPaneListener.java
+++ src/main/java/com/stream_pi/server/window/dashboard/actiondetailspane/ActionDetailsPaneListener.java
@@ -1,3 +1,18 @@
+/*
+ * Stream-Pi - Free & Open-Source Modular Cross-Platform Programmable Macropad
+ * Copyright (C) 2019-2021  Debayan Sutradhar (rnayabed),  Samuel Quiñones (SamuelQuinones)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
 package com.stream_pi.server.window.dashboard.actiondetailspane;
 
 import com.stream_pi.action_api.action.Action;
@@ -5,7 +20,8 @@ import com.stream_pi.server.window.dashboard.actiongridpane.ActionBox;
 import com.stream_pi.util.exception.MinorException;
 import javafx.stage.Window;
 
-public interface ActionDetailsPaneListener {
+public interface ActionDetailsPaneListener
+{
     void onActionClicked(Action action, ActionBox actionBox) throws MinorException;
 
     void saveAction(boolean runAsync, boolean runOnActionSavedFromServer);
